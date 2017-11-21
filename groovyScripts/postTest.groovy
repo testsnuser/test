080258fee8dc6644f35fe2ad94bdab22ebcb1c0b
tarun.murala <tarun.murala@servicenow.com>
2017-11-21 10:55:01 -0800

    STRY: Adding Surefire reports

--- groovyScripts/postTest.groovy
+++ groovyScripts/postTest.groovy
@@ -1,6 +1,10 @@
 //  @author tarun.murala
 
 import groovy.json.*
+import jenkins.model.*;
+import hudson.model.*;
+import hudson.tasks.*;
+import hudson.plugins.git.*;
 
 // manager.listener.logger.println "Into Post Test Script"
 
@@ -8,30 +12,28 @@ def SNOW_URL = "http://localhost:8080/api/now/test_integration_api"
 def SNOW_USER_NAME = "admin"
 def SNOW_USER_PASS = "admin"
 
-def json = new groovy.json.JsonBuilder()
-def testResultAction = manager.build.testResultAction
-def total = 0
-def failed = 0
-def skipped = 0
-def passed = 0
-if (testResultAction != null) {
-    total = testResultAction.totalCount
-    failed = testResultAction.failCount
-    skipped = testResultAction.skipCount
-    passed = total - failed - skipped
-}
-def jsonObj = json.build {
-    build_name manager.build.project.getName()
-    build_number manager.build.number
-    build_url manager.build.url
-    build_source "JENKINS"
-    branch_name manager.build.project.scm.branches[0].name
-    total_tests total
-    failed_tests failed
-    skipped_tests skipped
-    passed_tests passed 
-}
 
+def json = new groovy.json.JsonBuilder()
+def build = manager.build
+def testResultAction = build.testResultAction
+def jsonObj = json.build {
+    build_name build.project.getName()
+    build_number build.number
+    build_url build.url
+    build_source "JENKINS"
+    branch_name build.project.scm.branches[0].name
+}
+if (null != testResultAction) {
+    jsonObj.build.total_count = testResultAction.totalCount
+    jsonObj.build.failed_count = testResultAction.failCount
+    jsonObj.build.skipped_count = testResultAction.skipCount
+    jsonObj.build.passed_count = total - failed - skipped
+}
+def testAction = build.getAction(hudson.tasks.junit.TestResultAction.class);
+if(null != testAction) {
+    jsonObj.build.passed build.getAction(hudson.tasks.junit.TestResultAction.class).getPassedTests()
+    jsonObj.build.failed build.getAction(hudson.tasks.junit.TestResultAction.class).getFailedTests()    
+}
 // manager.listener.logger.println jsonObj.toString()
 
 def post = new URL(SNOW_URL).openConnection();
